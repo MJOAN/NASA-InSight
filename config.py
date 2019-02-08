f644842eadb7a40d9ca4807fd102c875dd647ed9
Mariam Joan <mariamjoan@Mariams-MacBook-Pro-2.local>
2019-02-07 23:20:46 -0800

    heroku

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'nothing_yet'
+    SECRET_KEY = ''
 
 class Production(Config):
     DEBUG = False
