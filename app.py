556af0ae1d50a910df49abd1190590c85ad9501f
Mariam Joan <mariamjoan@Mariams-MacBook-Pro-2.local>
2019-02-07 22:34:15 -0800

    twilio and images

--- app.py
+++ app.py
@@ -1,52 +1,60 @@
-# reference konradIT py library https://pypi.org/project/insight-api/
 from flask import Flask, request, render_template, flash, g, session, redirect, url_for
-from wtforms import Form, StringField, DateField, SubmitField, PhoneNumberField, validators
-from wtforms_components import Email, PhoneNumberField
+from wtforms import Form, StringField, IntegerField, DateField, SubmitField, validators
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+
+from insightmars import InSightAPI, utils
+InSightMission = InSightAPI()
+json_request = InSightMission.make_request()
+
+from twilio.twiml.messaging_response import MessagingResponse, Message
+from twilio.rest import Client
+import urllib
+
 import requests
 import json
 
-
+client = Client('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN')
 app = Flask(__name__, template_folder="templates")
 
-class ImageForm(Form):
-    date = DateField('date', format='%m-%d-%Y')
-    email = TextField('email', [validators.Length(min=6, max=35), validators=[Email()])
-    phone = PhoneNumberField('phone', validators=[country_code='FI', display_format='national'])
+class Notification(Form):
+    date = DateField('date', format='%m-%d-%Y') 
+    phone = IntegerField('phone')
+    email = StringField('email', [
+        Length(min=6, message=(u'please submit a valid email address')),
+        Email(message=('That\'s not a valid email address.'))
+    ])
     submit = SubmitField('submit')
-
-# "2014-12-10T16:44:31.486000Z"
-# date format = "yyyy-MM-dd'T'HH:mm:ss.SZ"
-# image = print(date["items"][0]["url"])
-# date = print(date["items"][0]["date_taken"] 
-# TODO: parse date user input 
 
 @app.route('/')
 def create_app():
     return render_template("index.html")
 
-@app.route('/get_images', methods=['GET'])
-def get_images(self, date):
-    date = request.form['date']
-    if date.validate_on_submit():
-        images = []
-        for i, image in enumerate(date["items"]):
-            if int(i) <= 10:
-                images.append(image["url"])
-        return images
+@app.route('/images', methods=['GET'])
+def images():
+    images = InSightMission.get_all(json_request)
+    # print(images, json_request)
+    return render_template("images.html", images=images)
 
 
-@app.route('/get_notification', methods=['POST']) # from date params
-def get_notification(self, email, phone):
+@app.route('/notification', methods=['POST']) # from date params
+def notification(email, phone):
     email = request.form['email']
     phone = request.form['phone']
+    message = client.messages \
+                .create(
+                     body="Thank you for registering to receive NASA Mars InSight Raw Image notifications! It is good to know we are going to stay in touch! Keep reaching for the stars! :)",
+                     from_='+12139557528',
+                     to=phone
+                 )
+    print(message.sid)
 
-
-    pass
-
-@app.route('/image/metadata') # show metadata 
-def get_metadata(self, metadata):
+@app.route('/metadata') # show metadata 
+def metadata():
+    # metadata = InSightMission.get_metadata(json_request, image_id)
     pass
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
